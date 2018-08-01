8e5f7675c6df10d2bdc4ccb04ccadce74b577c06
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-08-01 15:49:59 +0530

    Create mul.java

---
+++ mul.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=5;
+		for(int i=n;i<n+10;i++){
+	  if(i%10==0)
+	  System.out.print(i);
+	}
+}
+}
